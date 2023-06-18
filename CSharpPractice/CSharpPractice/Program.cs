6b7410369ef97ccb632aa87bac28030e64a8d4ea
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-17 22:04:53 -0400

    [Ruchita] Add: Welcome Message

---
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome welcome = new Welcome();
+            welcome.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpPractice/CSharpPractice/Welcome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    public class Welcome
+    {
+      public void Display()
+        {
+            Console.WriteLine("Welcome to C sharp Programming");
+
+        }
+      
+    }
+}
